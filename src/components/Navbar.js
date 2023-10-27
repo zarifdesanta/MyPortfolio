be99e0d843e635073431d6e51098a70b36db3f04
Zarif DeSanta <zarifdesanta@gmail.com>
2023-10-27 22:04:00 +0600

    changes-navbtn selection using useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,34 @@ import { Link } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import EngineeringIcon from '@mui/icons-material/Engineering';
 import SchoolIcon from '@mui/icons-material/School';
-import { Box, Tab, Tabs, Button, ButtonGroup } from '@mui/material';
+import { Button, ButtonGroup } from '@mui/material';
 import ZarfDevLogo from '../assets/ZarfDevLogo.png'
 
 //import createTheme and change color of buttons
 
 function Navbar() {
+
+  const [isSelectedHome, setHome] = useState(true);
+  const [isSelectedWorks, setWorks] = useState(false);
+  const [isSelectedInfo, setInfo] = useState(false);
+
+  function SelectHome(){
+    setHome(true);
+    setWorks(false);
+    setInfo(false);
+  }
+
+  function SelectWorks(){
+    setHome(false);
+    setWorks(true);
+    setInfo(false);
+  }
+
+  function SelectInfo(){
+    setHome(false);
+    setWorks(false);
+    setInfo(true);
+  }
 
   return (
     <div className='navbar'>
@@ -23,24 +45,33 @@ function Navbar() {
         </div>
         <div>
           <ButtonGroup variant="contained" size='small'>
-              <Link className='' to='/'>
-              <Button color="success" sx={{ borderRadius: 2, color: 'white', borderColor: 'white'}}
-                    startIcon={<HomeIcon></HomeIcon>}>
+            <Link className='' to='/'>
+              <Button color="success" sx={[ {borderRadius: 2}, 
+                    {color: 'white'}, 
+                    isSelectedHome ? {color: 'black'} : {color: 'white'}]}
+                    startIcon={<HomeIcon></HomeIcon>}
+                    onClick={() => SelectHome()}>
                   HOME
               </Button>
               </Link>
               <Link className='' to='/projects'>
-              <Button sx={{ borderRadius: 2, color: 'white', borderColor: 'white'}}
-                    startIcon={<EngineeringIcon></EngineeringIcon>}>
+                <Button color="info" sx={[ {borderRadius: 2}, 
+                    {color: 'white'}, 
+                    isSelectedWorks ? {color: 'black'} : {color: 'white'}]}
+                    startIcon={<EngineeringIcon></EngineeringIcon>}
+                    onClick={() => SelectWorks()}>
                   MY WORKS
-              </Button>
+                </Button>
               </Link>
               <Link className='' to='/academicInfo'>
-              <Button color='warning' sx={{ borderRadius: 2, color: 'white', borderColor: 'white'}}
-                    startIcon={<SchoolIcon></SchoolIcon>}>
-                  Academic Info
+              <Button color="warning" sx={[ {borderRadius: 2}, 
+                    {color: 'white'}, 
+                    isSelectedInfo ? {color: 'black'} : {color: 'white'}]}
+                    startIcon={<SchoolIcon></SchoolIcon>}
+                    onClick={() => SelectInfo()}>
+                  ACADEMIC INFO
               </Button>
-              </Link>
+            </Link>
           </ButtonGroup>
         </div>
     </div>
