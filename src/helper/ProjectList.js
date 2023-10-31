d17935a79b94a6ef5fc5b8c581b07226e4fd7c95
Zarif DeSanta <zarifdesanta@gmail.com>
2023-11-01 01:17:29 +0600

    changes-vangari project added

--- src/helper/ProjectList.js
+++ src/helper/ProjectList.js
@@ -1,81 +1,88 @@
-import Void from '../assets/void.png';
-import DHS from '../assets/dhs.png';
-import ZMS from '../assets/zms.png';
-import Portfolio from '../assets/my-portfolio.png';
-import OneBullet from '../assets/one_bullet.png';
-import FileAutomation from '../assets/file_auto.png';
-import Hyperchute from '../assets/hyperchute.png';
-import Towerone from '../assets/towerone.png';
-import Lostdungeon from '../assets/lost_dungeon.png';
-import Edge from '../assets/edge.png';
-import Picoto from '../assets/picoto.png';
+import Void from "../assets/void.png";
+import DHS from "../assets/dhs.png";
+import ZMS from "../assets/zms.png";
+import Portfolio from "../assets/my-portfolio.png";
+import OneBullet from "../assets/one_bullet.png";
+import FileAutomation from "../assets/file_auto.png";
+import Hyperchute from "../assets/hyperchute.png";
+import Towerone from "../assets/towerone.png";
+import Lostdungeon from "../assets/lost_dungeon.png";
+import Edge from "../assets/edge.png";
+import Picoto from "../assets/picoto.png";
+import Vangari from "../assets/vangari.png";
 
 export const ProjectList = [
-    {
-        image: Portfolio,
-        name: "MY PORTFOLIO",
-        tools: "REACT, JAVASCRIPT, MATERIAL UI, CSS",
-        link: ''
-    },
-    {
-        image: DHS,
-        name: "DESIGNER HIRING SYSTEM",
-        tools: "HTML, PHP, MATERIALIZE, CSS, MySQL",
-        link: 'https://github.com/zarifdesanta/DesignerHiringSystem/tree/main/DHS'
-    },
-    {
-        image: ZMS,
-        name: "ZOO MANAGEMENT SYSTEM",
-        tools: "HTML, PHP, MATERIALIZE, CSS, MySQL",
-        link: "https://github.com/zarifdesanta/ZooManagementSystem"
-    },
-    
-    {
-        image: FileAutomation,
-        name: "FILE AUTOMATION",
-        tools: "Python",
-        link: "https://github.com/zarifdesanta/FileAutomation"
-    },
-    {
-        image: OneBullet,
-        name: "ONE BULLET",
-        tools: "Python, PyGame",
-        link: "https://github.com/zarifdesanta/OneBullet"
-    },
-    {
-        image: Void,
-        name: "VOID",
-        tools: "Unity Engine, C#",
-        link: "https://zarifdesanta.itch.io/void"
-    },
-    {
-        image: Hyperchute,
-        name: "HYPERCHUTE",
-        tools: "Unity Engine, C#",
-        link: "https://zarifdesanta.itch.io/hyperchute"
-    },
-    {
-        image: Towerone,
-        name: "TOWER ONE",
-        tools: "Unity Engine, C#",
-        link: "https://zarifdesanta.itch.io/towerone"
-    },
-    {
-        image: Lostdungeon,
-        name: "LOST DUNGEON",
-        tools: "Unity Engine, C#",
-        link: "https://zarifdesanta.itch.io/lost-dungeon"
-    },
-    {
-        image: Edge,
-        name: "EDGE",
-        tools: "Unity Engine, C#",
-        link: "https://zarifdesanta.itch.io/edge"
-    },
-    {
-        image: Picoto,
-        name: "PICOTO",
-        tools: "Unity Engine, C#",
-        link: "https://zarifdesanta.itch.io/picoto"
-    },
+  {
+    image: Vangari,
+    name: "VANGARI",
+    tools: "REACT, JAVASCRIPT, MATERIAL UI, CSS",
+    link: "https://vangari.netlify.app/",
+  },
+  {
+    image: Portfolio,
+    name: "MY PORTFOLIO",
+    tools: "REACT, JAVASCRIPT, MATERIAL UI, CSS",
+    link: "https://zarfdev.netlify.app/",
+  },
+  {
+    image: DHS,
+    name: "DESIGNER HIRING SYSTEM",
+    tools: "HTML, PHP, MATERIALIZE, CSS, MySQL",
+    link: "https://github.com/zarifdesanta/DesignerHiringSystem/tree/main/DHS",
+  },
+  {
+    image: ZMS,
+    name: "ZOO MANAGEMENT SYSTEM",
+    tools: "HTML, PHP, MATERIALIZE, CSS, MySQL",
+    link: "https://github.com/zarifdesanta/ZooManagementSystem",
+  },
+
+  {
+    image: FileAutomation,
+    name: "FILE AUTOMATION",
+    tools: "Python",
+    link: "https://github.com/zarifdesanta/FileAutomation",
+  },
+  {
+    image: OneBullet,
+    name: "ONE BULLET",
+    tools: "Python, PyGame",
+    link: "https://github.com/zarifdesanta/OneBullet",
+  },
+  {
+    image: Void,
+    name: "VOID",
+    tools: "Unity Engine, C#",
+    link: "https://zarifdesanta.itch.io/void",
+  },
+  {
+    image: Hyperchute,
+    name: "HYPERCHUTE",
+    tools: "Unity Engine, C#",
+    link: "https://zarifdesanta.itch.io/hyperchute",
+  },
+  {
+    image: Towerone,
+    name: "TOWER ONE",
+    tools: "Unity Engine, C#",
+    link: "https://zarifdesanta.itch.io/towerone",
+  },
+  {
+    image: Lostdungeon,
+    name: "LOST DUNGEON",
+    tools: "Unity Engine, C#",
+    link: "https://zarifdesanta.itch.io/lost-dungeon",
+  },
+  {
+    image: Edge,
+    name: "EDGE",
+    tools: "Unity Engine, C#",
+    link: "https://zarifdesanta.itch.io/edge",
+  },
+  {
+    image: Picoto,
+    name: "PICOTO",
+    tools: "Unity Engine, C#",
+    link: "https://zarifdesanta.itch.io/picoto",
+  },
 ];
--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,46 +1,42 @@
-import React from 'react';
-import '../styles/MyProjects.css'
-import { ProjectList } from '../helper/ProjectList';
-import ProjectItem from '../components/ProjectItem';
-import Grid from '@mui/material/Unstable_Grid2/Grid2';
-import { Container } from '@mui/material';
+import React from "react";
+import "../styles/MyProjects.css";
+import { ProjectList } from "../helper/ProjectList";
+import ProjectItem from "../components/ProjectItem";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Container } from "@mui/material";
 
-import AnimatePage from '../components/AnimatePage';
+import AnimatePage from "../components/AnimatePage";
 
 function MyProjects() {
   return (
     <AnimatePage>
-    <div className=''>
-      <h1 className='name'>MY PROJECTS</h1>
-      <Container>
-        <Grid justifyContent='center' 
-                container
-                rowSpacing={3} 
-                columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-              
-        {
-          ProjectList.map((projectItem, key) => {
-            return (
-              <Grid>
-
-                <ProjectItem
-                  key = {key}
-                  image = {projectItem.image}
-                  name = {projectItem.name}
-                  tools = {projectItem.tools}
-                  link = {projectItem.link}>
-                </ProjectItem>
-              </Grid>
-              
-            )
-          }
-          )
-        }
-        </Grid>
-      </Container>
-    </div>
+      <div className="">
+        <h1 className="name">MY PROJECTS</h1>
+        <Container>
+          <Grid
+            justifyContent="center"
+            container
+            rowSpacing={3}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {ProjectList.map((projectItem, key) => {
+              return (
+                <Grid>
+                  <ProjectItem
+                    key={key}
+                    image={projectItem.image}
+                    name={projectItem.name}
+                    tools={projectItem.tools}
+                    link={projectItem.link}
+                  ></ProjectItem>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+      </div>
     </AnimatePage>
-  )
+  );
 }
 
-export default MyProjects
+export default MyProjects;
